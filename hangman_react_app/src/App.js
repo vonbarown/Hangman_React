4f2776541f56bce47286ee3b68014560850a93c9
vonbarown <vonielbrown@pursuit.org>
2019-12-21 21:26:47 -0500

    creating a single page multi-component app

--- hangman_react_app/src/App.js
+++ hangman_react_app/src/App.js
@@ -1,66 +1,30 @@
 import React from 'react';
 import './App.css';
-import words from './components/words'
-import Game from './components/Game'
+import { Link, Switch, Route } from 'react-router-dom';
+import gameApp from './components/gameApp'
+import Home from './Home.jsx'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      wordArr: words,
-      word: '',
-      splitWord: [],
-      hiddenWord: [],
-      maxTries: 10,
+
     }
   }
-
-
-
-  componentDidMount() {
-    this.randomGen()
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.word !== prevState.word) {
-      this.underScore()
-    }
-  }
-
-  randomGen = () => {
-    const { wordArr } = this.state
-    let generatedWord = wordArr[Math.floor(Math.random() * words.length)];
-    this.setState({
-      word: generatedWord,
-      splitWord: generatedWord.split('')
-    })
-
-  }
-
-  underScore = () => {
-    let { splitWord } = this.state
-
-    let wordUnderscore = []
-    splitWord.forEach(el => {
-      wordUnderscore.push("_")
-    })
-
-    this.setState({
-      hiddenWord: wordUnderscore
-    })
-  }
-
 
   render() {
-    console.log('App state', this.state);
-    const { splitWord, hiddenWord } = this.state
+
 
     return (
       <div className="App">
-        <div className='title'>
-          <h1>HangMan</h1>
-        </div>
-        <Game splitWord={splitWord} hiddenWord={hiddenWord} />
+        <nav>
+          <Link to="/">Home</Link>{" "}
+          <Link to="/hangman/general">General Game</Link>{" "}
+        </nav>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/hangman/general' component={gameApp}></Route>
+        </Switch>
       </div>
     );
   }
---
+++ hangman_react_app/src/components/gameApp.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import words from './words'
+import Game from './Game'
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            wordArr: words,
+            word: '',
+            splitWord: [],
+            hiddenWord: [],
+            maxTries: 10,
+        }
+    }
+
+
+
+    componentDidMount() {
+        this.randomGen()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.word !== prevState.word) {
+            this.underScore()
+        }
+    }
+
+    randomGen = () => {
+        const { wordArr } = this.state
+        let generatedWord = wordArr[Math.floor(Math.random() * words.length)];
+        this.setState({
+            word: generatedWord,
+            splitWord: generatedWord.split('')
+        })
+
+    }
+
+    underScore = () => {
+        let { splitWord } = this.state
+
+        let wordUnderscore = []
+        splitWord.forEach(el => {
+            wordUnderscore.push("_")
+        })
+
+        this.setState({
+            hiddenWord: wordUnderscore
+        })
+    }
+
+
+    render() {
+        console.log('App state', this.state);
+        const { splitWord, hiddenWord } = this.state
+
+        return (
+            <div className="App">
+                <div className='title'>
+                    <h1>HangMan</h1>
+                </div>
+                <Game splitWord={splitWord} hiddenWord={hiddenWord} />
+            </div>
+        );
+    }
+}
+
+export default App;
