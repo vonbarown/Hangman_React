fb713c973592b1841dc6583a76c5e5b51a28c2c5
vonbarown <vonielbrown@pursuit.org>
2020-05-21 17:13:07 -0400

    reconfiguring  game logic and styling

--- hangman_react_app/src/components/Display.jsx
+++ hangman_react_app/src/components/Display.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import './game.css'
-const Display = (props) => {
-    const { hiddenWord, rightGuesses, wrongGuesses, maxTries, message, generateWord } = props
+import Reactfrom "react";
+import "./game.css";
 
-
-    return (<div className='display'>
-        <p>You hav {maxTries} chances remaining</p>
-        <p>{hiddenWord.join(' ')}</p>
-        <div className='guesses'>
-            <div className='right'>
-                <p>Right Guesses</p>
-                <p>{rightGuesses.join(' ')}</p>
-            </div>
-            <div className='wrong'>
-                <p>Wrong Guesses</p>
-                <p>{wrongGuesses}</p>
-            </div>
-            <p>{message}</p>
-            <button onClick={generateWord}>Reload</button>
+const Display = ({
+  hiddenWord,
+  rightGuesses,
+  wrongGuesses,
+  maxTries,
+  message,
+  generateWord,
+}) => {
+  return (
+    <div className="display">
+      <p>You hav {maxTries} chances remaining</p>
+      <p>{hiddenWord.join(" ")}</p>
+      <div className="guesses">
+        <div className="right">
+          <p>Right Guesses</p>
+          <p>{rightGuesses.join(" ")}</p>
         </div>
+        <div className="wrong">
+          <p>Wrong Guesses</p>
+          <p>{wrongGuesses}</p>
+        </div>
+        <p>{message}</p>
+      </div>
+      <button className="restart" onClick={generateWord}>
+        Restart
+      </button>
     </div>
-    )
+  );
+};
 
-}
-
-export default Display
+export default Display;
--- hangman_react_app/src/components/Game.jsx
+++ hangman_react_app/src/components/Game.jsx
@@ -1,122 +1,123 @@
-import React from 'react';
-import { toast } from 'react-toastify';
-import Display from './Display';
-
+import React from "react";
+import { toast } from "react-toastify";
+import Display from "./Display";
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props)
-        this.initialState = {
-            key: '',
-            message: '',
-            maxTries: 10,
-            rightGuesses: [],
-            wrongGuesses: [],
-        }
-        this.state = this.initialState
-    }
+  state = {
+    key: "",
+    message: "",
+    maxTries: 6,
+    rightGuesses: [],
+    wrongGuesses: [],
+  };
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.key !== prevState.key) {
-            this.validity()
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.key !== prevState.key) {
+      this.validity();
     }
+  }
 
-    handleInput = e => {
+  handleInput = (e) => {
+    this.setState({
+      key: String.fromCharCode(e.charCode),
+    });
+  };
+
+  //keeps track of the users right guesses
+  checkRightGuess = (userInput) => {
+    let { rightGuesses } = this.state;
+    if (!rightGuesses.includes(userInput)) {
+      rightGuesses.push(userInput);
+      this.setState({
+        rightGuesses: rightGuesses,
+      });
+    }
+  };
+
+  //keeping track of the users wrong guesses
+  checkWrongGuess = (userInput) => {
+    let { wrongGuesses } = this.state;
+    if (!wrongGuesses.includes(userInput) && userInput !== "") {
+      wrongGuesses.push(userInput);
+      this.setState({
+        wrongGuesses: wrongGuesses,
+      });
+    }
+  };
+
+  validity = () => {
+    const { splitWord, hiddenWord } = this.props;
+    let { key, maxTries } = this.state;
+
+    for (let l = 0; l < splitWord.length; l++) {
+      //checking if the letter is inside of the array and the space contains an underscore
+      if (
+        splitWord[l].toLowerCase() === key.toLowerCase() &&
+        hiddenWord[l] === "_" &&
+        key.toLowerCase() !== ""
+      ) {
+        hiddenWord[l] = key.toLowerCase();
+        // // this function ensures that the letter is not duplicated in the array
+        this.checkRightGuess(key.toLowerCase());
+        // // creating a win condition  to informs the user that they have won
+        if (splitWord.join("") === hiddenWord.join("")) {
+          this.setState({
+            message: `Congrats! You won`,
+          });
+          toast.success(`Congrats! You won`);
+        }
+      }
+
+      if (maxTries === 0) {
         this.setState({
-            key: String.fromCharCode(e.charCode)
-        })
-    }
-
-    checkRightGuess = (userInput) => {
-        let { rightGuesses } = this.state
-        if (!rightGuesses.includes(userInput)) {
-            rightGuesses.push(userInput)
-            this.setState({
-                rightGuesses: rightGuesses
-            })
+          message: `You lost. Press button to restart.`,
+          maxTries: 0,
+        });
+        return toast.error(`You lost. Press button to restart.`);
+      } else {
+        if (
+          !splitWord.includes(key.toLowerCase()) &&
+          key.toLowerCase() !== ""
+        ) {
+          this.setState({
+            maxTries: maxTries - 1,
+          });
+          this.checkWrongGuess(key.toLowerCase());
         }
+      }
     }
+  };
 
-    checkWrongGuess = (userInput) => {
-        let { wrongGuesses } = this.state
-        if (!wrongGuesses.includes(userInput) && userInput !== '') {
-            wrongGuesses.push(userInput)
-            this.setState({
-                wrongGuesses: wrongGuesses
-            })
-        }
-    }
+  generateWord = () => {
+    this.props.randomGen();
+    this.setState({
+      key: "",
+      rightGuesses: [],
+      wrongGuesses: [],
+      message: "",
+      maxTries: 10,
+    });
+  };
 
-    validity = () => {
-        const { splitWord, hiddenWord } = this.props
-        let { key, maxTries } = this.state
+  render() {
+    console.log("Game state", this.state);
 
-        for (let l = 0; l < splitWord.length; l++) {
-            //checking if the letter is inside of the array and the space contains an underscore
-            if (splitWord[l].toLowerCase() === key.toLowerCase() && hiddenWord[l] === "_" && key.toLowerCase() !== '') {
-                hiddenWord[l] = key.toLowerCase();
-                // // this function ensures that the letter is not duplicated in the array
-                this.checkRightGuess(key.toLowerCase())
-                // // creating a win condition  to informs the user that they have won
-                if (splitWord.join('') === hiddenWord.join('')) {
-                    this.setState({
-                        message: `Congrats! You won`
-                    })
-                    toast.success(`Congrats! You won`)
-                }
-            }
-
-            if (!splitWord.includes(key.toLowerCase()) && key.toLowerCase() !== '') {
-                this.setState({
-                    maxTries: maxTries - 1
-                })
-                this.checkWrongGuess(key.toLowerCase())
-            }
-
-            if (maxTries === 0) {
-                this.setState({
-                    message: `You lost. Press button to restart.`,
-                    maxTries: 0
-                })
-                toast.error(`You lost. Press button to restart.`)
-                return
-            }
-        }
-
-    }
-
-    generateWord = () => {
-        this.props.randomGen()
-        this.setState({
-            key: '',
-            rightGuesses: [],
-            wrongGuesses: [],
-            message: '',
-            maxTries: 10
-        })
-    }
-
-    render() {
-        console.log('Game state', this.state);
-
-        let { rightGuesses, wrongGuesses, maxTries, sound, message } = this.state
-        // const { generateWord } = this.props
-        return (
-            <div className='game'>
-                <input type="text" onKeyPress={this.handleInput} />
-                <Display
-                    hiddenWord={this.props.hiddenWord}
-                    rightGuesses={rightGuesses}
-                    wrongGuesses={wrongGuesses}
-                    maxTries={maxTries}
-                    sound={sound}
-                    message={message}
-                    generateWord={this.generateWord}
-                />
-            </div>
-        )
-    }
+    let { rightGuesses, wrongGuesses, maxTries, sound, message } = this.state;
+    return (
+      <div className="game">
+        <input type="text" onKeyPress={this.handleInput} />
+        <Display
+          hiddenWord={this.props.hiddenWord}
+          rightGuesses={rightGuesses}
+          wrongGuesses={wrongGuesses}
+          maxTries={maxTries}
+          sound={sound}
+          message={message}
+          generateWord={this.generateWord}
+        />
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
